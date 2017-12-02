8b8853889accd5282ad197839ded5daa753e23d0
Martin R-L <martin.rosenlidholm@gmail.com>
2017-12-02 17:22:42 +0100

    add boilerplate test proj

---
+++ StraightOutTheStrait.Tests/SplitTheStraitTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Xbehave;
+using Xunit;
+
+namespace StraightOutTheStrait.Tests
+{
+    public class SplitTheStraitTests
+    {
+        [Scenario]
+        public void Addition(int x, int y, Calculator calculator, int answer)
+        {
+            "Given the number 1"
+                .x(() => x = 1);
+
+            "And the number 2"
+                .x(() => y = 2);
+
+            "And a calculator"
+                .x(() => calculator = new Calculator());
+
+            "When I add the numbers together"
+                .x(() => answer = calculator.Add(x, y));
+
+            "Then the answer is 3"
+                .x(() => Assert.Equal(3, answer));
+        }
+    }
+    
+    public class Calculator
+    {
+        public int Add(int x, int y) => x + y;
+    }
+}
