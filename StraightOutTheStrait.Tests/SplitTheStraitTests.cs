b6576f11bea0207e07ddc7ceabe3893a0c964e75
Martin R-L <martin.rosenlidholm@gmail.com>
2017-12-03 15:25:39 +0100

    add unit test

--- StraightOutTheStrait.Tests/SplitTheStraitTests.cs
+++ StraightOutTheStrait.Tests/SplitTheStraitTests.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.IO;
+using System.Xml.Linq;
 using Xbehave;
 using Xunit;
 
@@ -8,27 +8,34 @@ namespace StraightOutTheStrait.Tests
     public class SplitTheStraitTests
     {
         [Scenario]
-        public void Addition(int x, int y, Calculator calculator, int answer)
+        public void Split(
+            XDocument faultyCommuteWithIncludedStrait,
+            StraitSplitter straitSplitter,
+            XDocument swedishRide,
+            XDocument danishRide)
         {
-            "Given the number 1"
-                .x(() => x = 1);
+            "Given the faulty commute"
+                .x(() => faultyCommuteWithIncludedStrait = XDocument.Parse(File.ReadAllText(@"GPX-files\original.gpx")));
 
-            "And the number 2"
-                .x(() => y = 2);
+            "And a strait splitter"
+                .x(() => straitSplitter = new StraitSplitter());
+                
+            "When I split the faulty commute"
+                .x(() => (swedishRide, danishRide) = straitSplitter.Split(faultyCommuteWithIncludedStrait));
 
-            "And a calculator"
-                .x(() => calculator = new Calculator());
-
-            "When I add the numbers together"
-                .x(() => answer = calculator.Add(x, y));
-
-            "Then the answer is 3"
-                .x(() => Assert.Equal(3, answer));
+            "Then the Swedish ride is the Swedish legs"
+                .x(() => Assert.Equal(XDocument.Parse(File.ReadAllText(@"GPX-files\Sweden-lon12.9.gpx")), swedishRide));
+            
+            "Then the Danish ride is the Danish legs"
+                .x(() => Assert.Equal(XDocument.Parse(File.ReadAllText(@"GPX-files\Denmark-lon12.6.gpx")), danishRide));
         }
-    }
-    
-    public class Calculator
-    {
-        public int Add(int x, int y) => x + y;
+
+        public class StraitSplitter
+        {
+            public (XDocument swedishRide, XDocument danishRide) Split(XDocument faultyCommuteWithIncludedStrait)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
     }
 }
