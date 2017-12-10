897e2f9b5ad2fb79d2732b7ca6f52496cfcf7b24
Martin R-L <martin.rosenlidholm@gmail.com>
2017-12-10 20:40:51 +0100

    copy splitter

--- SplitTheStrait/Program.cs
+++ SplitTheStrait/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace SplitTheStrait
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+    
+    public class StraitSplitter
+    {
+        public void Split(XDocument faultyCommuteWithIncludedStrait)
+        {
+            var swedishRide = new XDocument(faultyCommuteWithIncludedStrait);
+            var danishRide = new XDocument(faultyCommuteWithIncludedStrait);
+                
+            swedishRide
+                .Descendants()
+                .Where(_ =>
+                {
+                    bool swedishBorderLon;
+                    try
+                    {
+                        swedishBorderLon = decimal.Parse(_.Attribute("lon").Value) < 12.9m;
+                    }
+                    catch
+                    {
+                        swedishBorderLon = false;
+                    }
+
+                    return swedishBorderLon;
+                }).Remove();
+                
+            danishRide
+                .Descendants()
+                .Where(_ =>
+                {
+                    bool danishBorderLon;
+                    try
+                    {
+                        danishBorderLon = decimal.Parse(_.Attribute("lon").Value) > 12.7m;
+                    }
+                    catch
+                    {
+                        danishBorderLon = false;
+                    }
+
+                    return danishBorderLon;
+                }).Remove();
+                
+            danishRide.Descendants().First(_ => _.Name.LocalName == "time").Value =
+                danishRide.Descendants().Where(_ => _.Name.LocalName == "time").ElementAt(1).Value;
+                
+            swedishRide.Save("swedish_leg.gpx");
+            danishRide.Save("danish_leg.gpx");
         }
     }
 }
